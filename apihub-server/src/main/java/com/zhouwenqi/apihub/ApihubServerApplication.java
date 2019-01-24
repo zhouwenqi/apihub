54ec8706f1f448c9fe6035647f73ddc3ac2892cb
zhouwenqi <zhouwenqi1982@live.com>
2019-01-24 11:25:39 +0800

    初始化项目

---
+++ apihub-server/src/main/java/com/zhouwenqi/apihub/ApihubServerApplication.java
@@ -0,0 +1,14 @@
+package com.zhouwenqi.apihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihubServerApplication.class, args);
+	}
+
+}
+
